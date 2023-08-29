fef3f9feb00e94b5f91c42db4f00d15296091ac0
Mohit Sahu <63452333+mohit643@users.noreply.github.com>
2023-08-29 06:32:38 +0000

    done

---
+++ class_01/dataType.js
@@ -0,0 +1,26 @@
+// alert(3+3);
+
+// its gave Error beacuse its run to node js and its properly run to broweser //
+
+let accountName = "MOHIT";
+let accountId = 100;
+let login = true/false;
+
+    // DATA TYPES 
+
+/*
+number = 12
+bigInt = its use to store max no
+string = ""
+boolean = true/false
+null = standalone value
+undefined = undefine
+symbol = unique
+
+
+object
+*/
+
+console.log(typeof accountName); // string
+console.log(typeof null); //object
+console.log(typeof undefined); // undefiend
---
+++ class_01/variable.js
@@ -0,0 +1,25 @@
+const accountId = 100;
+let accountName = "Mohit sahu";
+var accountEmail = "sahumohit643@gmail.com";
+accountCity = "Fatehpur";
+
+// const is constable value //
+
+accountName = "Rohit sahu";
+accountEmail = "sahurohit159@gmail.com";
+accountCity = "Noida";
+let accountNumber ;
+
+console.log(accountName);
+console.table([accountEmail,accountId,accountName,accountCity,accountNumber])
+
+/*
+{
+    its called scope.
+}
+*/
+
+/*
+don't use var 
+beacuse of issue in block scope and functional scope
+*/
