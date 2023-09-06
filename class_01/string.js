62e20d2daa41d5f0c087a4cf9ffd64220b505597
Mohit Sahu <63452333+mohit643@users.noreply.github.com>
2023-09-06 09:07:14 +0000

    done string

---
+++ class_01/string.js
@@ -0,0 +1,9 @@
+let Name = "  mohit   ";
+console.log(Name); //  mohit   
+console.log(Name.trim()); //mohit
+
+let url = "http//ijhsu@!huhnjn.com";
+console.log(url.replace("@!","-"));// http//ijhsu-huhnjn.com
+
+let GameName = "Mohit-sahu";
+console.log(GameName.split("-")); // ["Mohit","sahu"]
