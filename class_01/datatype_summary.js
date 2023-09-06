08191f6f2121de38c69a24f028916f738cb9b962
Mohit Sahu <63452333+mohit643@users.noreply.github.com>
2023-09-06 07:27:34 +0000

    DataType Done

---
+++ class_01/datatype_summary.js
@@ -0,0 +1,13 @@
+// datatype is basicly two types 
+// 1) premitive datatype
+// 2) Non premitive datatype
+
+// 1) premitive DataType => String,Number,BigInt,Boolean,undefined,null
+// 2) Non primitive datatype => Array,Object, function
+
+
+//  Its is object 
+// {
+//     name:mohit;    
+//     age:23;
+// }
