66768d5571a6d312ac1363086923089e59fe5413
Mohit Sahu <63452333+mohit643@users.noreply.github.com>
2023-09-06 11:08:00 +0000

    done

---
+++ class_01/date.js
@@ -0,0 +1,7 @@
+const date = new Date();
+console.log(date);
+console.log(date.toDateString());
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleString());
+const myTime = Date.now();
+console.log(myTime);//1693997512897 
